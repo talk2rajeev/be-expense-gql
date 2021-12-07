a493e898967f69de65b59c9fe4b2fe1b409003e1
Rajeev Sharma <talk2rajeev1986@gmail.com>
2021-12-07 10:21:15 +0530

    mock database added

--- gql-typescript-server/database/mockdata.ts
+++ gql-typescript-server/database/mockdata.ts
@@ -10,3 +10,52 @@ export const users = [
       projects: [{title: 'site upgrade - summer 2021'}]
     },
 ];
+
+export const vendorList = [
+  {
+      id: '4mt4t4--te4asdf-edfgdfg',
+      name: 'Sangam Thekedar',
+      type: 'Work',
+      address: '',
+  },
+  {
+      id: 't1err323--tswrdfgf-tyrhsg',
+      name: 'VIP Trading',
+      type: 'Material',
+      address: 'Marwa',
+  },
+  {
+      id: '4t324t--65ue6eu6-7e65ie56',
+      name: 'Sangay Enterprise',
+      type: 'Material',
+      address: '',
+  },
+];
+
+export const ExpenseList = [
+  {
+      id: '3yw5yw5y3-herh5ea3-w45wh6ueu',
+      expname: 'Mason work',
+      exptype: 'labor',
+      amount: 1500,
+      date: 'Thu Oct 19 2021',
+      vendorId: '4mt4t4--te4asdf-edfgdfg'
+
+  },
+  {
+      id: '45y45ey-56u34tq-78y3qudt6erg',
+      expname: 'Balu',
+      exptype: 'Material',
+      amount: 50000,
+      date: 'Mon Nov 15 2021',
+      vendorId: '4t324t--65ue6eu6-7e65ie56'
+  },
+  {
+      id: 'a9pert9-43p9utq4-34y4hoiwfg',
+      expname: 'Steel',
+      exptype: 'Material',
+      amount: 24500,
+      date: 'Fri Dec 25 2021',
+      vendorId: 't1err323--tswrdfgf-tyrhsg'
+  },
+];
